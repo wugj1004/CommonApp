98ea8a2099d926e98f046b9619f7afab9ab765ce
wugj <wugj@qq.com>
2019-10-17 16:47:37 +0800

    订阅父类事件，默认也可以接受消息

--- business/compmain/src/main/java/com/wugj/compmain/EventBusActivity.kt
+++ business/compmain/src/main/java/com/wugj/compmain/EventBusActivity.kt
@@ -1,12 +1,12 @@
 package com.wugj.compmain
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
-
 import com.wugj.common.BaseActivity
-import com.wugj.compmain.eventbus1.MyEventBus
-
+import com.wugj.compmain.event.BaseEvent
+import com.wugj.compmain.event.MyEventBus
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
@@ -27,28 +27,69 @@ class EventBusActivity : BaseActivity() {
         setContentView(R.layout.main_activity_event)
 
 
+
+
+
     }
 
 
     fun postEvent(v :View){
-        EventBus.getDefault().post(MyEventBus("123456789"))
+//        EventBus.getDefault().post(MyEventBus("主线程发送事件"))
+//
+        object:Thread(){
+            override fun run() {
+                super.run()
+                EventBus.getDefault().post(MyEventBus("子程发送事件"))
+            }
+        }.start()
+
     }
 
     override fun onStart() {
         super.onStart()
+        //注册事件
+        EventBus.getDefault().register(this@EventBusActivity)
 
-        EventBus.getDefault().register(this)
     }
 
 
     override fun onDestroy() {
         super.onDestroy()
+        //注销事件
         EventBus.getDefault().unregister(this)
     }
 
+    //订阅事件
     //主线程
-    @Subscribe(threadMode = ThreadMode.MAIN,  sticky =  true, priority = 100)
-    internal fun onMessageEvent(event: MyEventBus) {
-        Toast.makeText(this, event.messagge, Toast.LENGTH_SHORT).show()
+    @Subscribe(threadMode = ThreadMode.POSTING,  sticky =  false, priority = 100)
+    internal fun onSubscriber(event: MyEventBus) {
+
+        Log.e(EventBusActivity::class.java.simpleName+"111",event.messagge)
+
+        runOnUiThread {
+
+            Toast.makeText(this,"喜爱",Toast.LENGTH_SHORT).show()
+        }
+        //阻塞事件传递
+        EventBus.getDefault().cancelEventDelivery(event)
     }
+
+    @Subscribe(threadMode = ThreadMode.BACKGROUND,  sticky =  false, priority = 50)
+    internal fun onSubscriber1(event: MyEventBus) {
+        Log.e(EventBusActivity::class.java.simpleName+"222",event.messagge)
+    }
+
+
+    @Subscribe(threadMode = ThreadMode.MAIN,  sticky =  false, priority = 10)
+    internal fun onSubscriber2(event: BaseEvent) {
+        Log.e(EventBusActivity::class.java.simpleName+"333",event.priority.toString())
+    }
+
+
+
+
+
+
+
+
 }
